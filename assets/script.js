e09b648b297415f3a30072a9ad83f96b094daef1
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-07-03 20:24:34 +0430

    ♻️ refactor : Refactored drags and drops.

--- assets/script.js
+++ assets/script.js
@@ -1,60 +1,32 @@
-let currentDroppable = null;
-
-ball.onmousedown = function (event) {
-  let shiftX = event.clientX - ball.getBoundingClientRect().left;
-  let shiftY = event.clientY - ball.getBoundingClientRect().top;
-
-  ball.style.position = "absolute";
-  ball.style.zIndex = 1000;
-  document.body.append(ball);
-
-  moveAt(event.pageX, event.pageY);
-
-  function moveAt(pageX, pageY) {
-    ball.style.left = pageX - shiftX + "px";
-    ball.style.top = pageY - shiftY + "px";
+let mouseOffset = { x: 0, y: 0 };
+let isMouseDown = false;
+const onMouseDown = function (e) {
+  if (e.target.classList.contains("list")) {
+    console.log("mouseDown", e.target);
+    isMouseDown = true;
+    mouseOffset = {
+      x: e.target.offsetLeft - e.clientX,
+      y: e.target.offsetTop - e.clientY,
+    };
   }
-
-  function onMouseMove(event) {
-    moveAt(event.pageX, event.pageY);
-
-    ball.hidden = true;
-    let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-    ball.hidden = false;
-
-    if (!elemBelow) return;
-
-    let droppableBelow = elemBelow.closest(".droppable");
-    if (currentDroppable != droppableBelow) {
-      if (currentDroppable) {
-        // null when we were not over a droppable before this event
-        leaveDroppable(currentDroppable);
-      }
-      currentDroppable = droppableBelow;
-      if (currentDroppable) {
-        // null if we're not coming over a droppable now
-        // (maybe just left the droppable)
-        enterDroppable(currentDroppable);
-      }
-    }
-  }
-
-  document.addEventListener("mousemove", onMouseMove);
-
-  ball.onmouseup = function () {
-    document.removeEventListener("mousemove", onMouseMove);
-    ball.onmouseup = null;
-  };
 };
 
-function enterDroppable(elem) {
-  elem.style.background = "pink";
-}
-
-function leaveDroppable(elem) {
-  elem.style.background = "";
-}
-
-ball.ondragstart = function () {
-  return false;
+const onMouseMove = function (e) {
+  e.preventDefault();
+  if (isMouseDown) {
+    e.target.style.left = e.clientX + mouseOffset.x + "px";
+    e.target.style.top = e.clientY + mouseOffset.y + "px";
+  }
 };
+
+const onMouseUp = function (e) {
+  isMouseDown = false;
+};
+
+(function () {
+  const lists = document.querySelector(".lists");
+  const groups = lists;
+  lists.addEventListener("mousedown", onMouseDown);
+  lists.addEventListener("mousemove", onMouseMove);
+  lists.addEventListener("mouseup", onMouseUp);
+})();
